3048bed32a09f70d92cde141a1c885283d7c5b35
ANUSHA <anushaganeshan06@gmail.com>
2024-11-22 17:37:13 +0530

    commit

---
+++ Modules and packages/module_01/one.py
@@ -0,0 +1,13 @@
+import two
+
+two.function2() # 2 imported
+
+def function():
+    print("function printed ")
+
+print("outer side printer")
+    
+if __name__ == "__main__":
+    print("the name are same")
+else:
+    print('the name are not same')
---
+++ Modules and packages/module_01/two.py
@@ -0,0 +1,12 @@
+# import one
+
+# one.function() # cant import same modile again in again
+
+def function2():
+    print("2 modules imported")
+
+
+if __name__ == "__main__":
+    print("the two modules name are equal")
+else:
+    print("the two module name are not equal")
