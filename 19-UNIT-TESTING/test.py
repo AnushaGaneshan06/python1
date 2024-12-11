eed01610c24c5ca6992ba4c377e2d56adbd65a6b
ANUSHA <anushaganeshan06@gmail.com>
2024-12-11 19:02:22 +0530

    commit

---
+++ 19-UNIT-TESTING/test.py
@@ -0,0 +1,22 @@
+
+
+# PYLINT IS USED TO RATE THE QUALITY OF THE CODE
+
+
+"""
+A very simple script.
+"""
+
+
+def myfunc():
+    """
+    An extremely simple function.
+    """
+    first = 1
+    second = 2
+    thrid = 5
+    print(first)
+    print(second)
+    print(thrid)
+
+myfunc()
