c6e6e075d63659fdd8a53816d409b8f20bfaa44b
ANUSHA <anushaganeshan06@gmail.com>
2024-12-12 20:14:20 +0530

    commit

--- 19-UNIT-TESTING/test.py
+++ 19-UNIT-TESTING/test.py
@@ -1,22 +1,22 @@
+import unittest
+import test_cap  # Import the module containing the function to test
+
+# Define a test case class that inherits from unittest.TestCase
+class Testcap(unittest.TestCase):
+
+    # Define a test method using the standard naming convention `test_`
+    def test_add(self):
+
+        # Call the function from the imported module and store the result
+        result = test_cap.add(10, 15)
+
+        # Use an assertion to check if the result matches the expected value
+        # ----------------
+        # test case 1:
+        self.assertEqual(result, 25)
 
 
-# PYLINT IS USED TO RATE THE QUALITY OF THE CODE
 
-
-"""
-A very simple script.
-"""
-
-
-def myfunc():
-    """
-    An extremely simple function.
-    """
-    first = 1
-    second = 2
-    thrid = 5
-    print(first)
-    print(second)
-    print(thrid)
-
-myfunc()
+# Standard condition to allow the test to run directly when the file is executed
+if __name__ == "__main__":
+    unittest.main()
---
+++ 19-UNIT-TESTING/test_cap.py
@@ -0,0 +1,24 @@
+# creating the test cases in the another modules
+
+def add(a, b):
+    return a + b
+
+
+# def substract(a, b):
+#     return a - b
+
+# def multiple(a, b):
+#     return a * b
+
+# def divide(a, b):
+#     try:
+#         if b == 0:
+#             raise ValueError
+#         return a % b
+#     except ValueError:
+#         print("no zero")
+
+    
+
+
+
